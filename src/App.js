c7c4505cfce9a59ededf8bfb255dfc9e30a7ca03
AkinduShenal <146591779+AkinduShenal@users.noreply.github.com>
2025-01-21 22:58:33 +0530

    Add Nav Bar to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import ProductsPage from './pages/ProductsPage';
+import CategoriesPage from './pages/CategoriesPage';
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to Product Management System</h1>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/categories" element={<CategoriesPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
