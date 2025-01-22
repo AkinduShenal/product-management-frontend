6812f6364f56c6907b96b80e9528e18330f7ec44
AkinduShenal <146591779+AkinduShenal@users.noreply.github.com>
2025-01-22 14:38:14 +0530

    Adding Styles to nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Navbar = () => (
-  <nav>
-    <ul>
-      <li><Link to="/categories">Categories</Link></li>
-      <li><Link to="/products">Products</Link></li>
-    </ul>
+  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div className="container">
+      <Link className="navbar-brand" to="/">MyApp</Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/categories">Categories</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/products">Products</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
 );
 
